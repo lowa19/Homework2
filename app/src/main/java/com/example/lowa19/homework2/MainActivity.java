5e6bc295d1db67c771ea0d082af9ec55f9d67c12
lowa19 <lowa19@up.edu>
2017-03-23 17:44:34 -0700

    graphics finished started registering listeners

--- app/src/main/java/com/example/lowa19/homework2/MainActivity.java
+++ app/src/main/java/com/example/lowa19/homework2/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.lowa19.homework2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    myCustomPicture myPictureLayout;
+    private SeekBar redSeekBar, greenSeekBar, blueSeekBar;
+    private TextView redText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        myPictureLayout = new myCustomPicture(this);
+        setContentView(myPictureLayout);
+        //setContentView(R.layout.activity_main);
+
+        redSeekBar = (SeekBar) findViewById(R.id.rSeekBar);
+        redText = (TextView) findViewById(R.id.redText);
+        redText.setText("Red Value = " + redSeekBar.getProgress());
+
+        //redSeekBar.setOnSeekBarChangeListener();
+
+
     }
 }
--- app/src/main/java/com/example/lowa19/homework2/myCustomPicture.java
+++ app/src/main/java/com/example/lowa19/homework2/myCustomPicture.java
@@ -12,38 +12,80 @@ import android.view.SurfaceView;
  */
 public class myCustomPicture extends SurfaceView {
     Paint black_paintbrush_fill, yellow_paintbrush_fill, white_paintbrush_fill;
-    Path triangle;
+    Path nose, leftFoot, rightFoot, leftArm, rightArm;
+
     public myCustomPicture(Context context) {
         super(context);
+        setWillNotDraw(false);
     }
-}
 
-@Override
-public void onDraw(Canvas canvas)
-{
-    black_paintbrush_fill = new Paint();
-    black_paintbrush_fill.setColor(Color.BLACK);
-    black_paintbrush_fill.setStyle(Paint.Style.FILL);
 
-    yellow_paintbrush_fill = new Paint();
-    yellow_paintbrush_fill.setColor(Color.YELLOW);
-    yellow_paintbrush_fill.setStyle(Paint.Style.FILL);
+    @Override
+    public void onDraw(Canvas canvas) {
+        black_paintbrush_fill = new Paint();
+        black_paintbrush_fill.setColor(Color.BLACK);
+        black_paintbrush_fill.setStyle(Paint.Style.FILL);
 
-    white_paintbrush_fill = new Paint();
-    white_paintbrush_fill.setColor(Color.WHITE);
-    white_paintbrush_fill.setStyle(Paint.Style.FILL);
+        yellow_paintbrush_fill = new Paint();
+        yellow_paintbrush_fill.setColor(Color.YELLOW);
+        yellow_paintbrush_fill.setStyle(Paint.Style.FILL);
 
-    canvas.drawCircle(100, 100, 50, black_paintbrush_fill); //body
-    canvas.drawCircle(125, 125, 5, white_paintbrush_fill); //left eye
-    canvas.drawCircle(175, 125, 5, white_paintbrush_fill); //right eye
+        white_paintbrush_fill = new Paint();
+        white_paintbrush_fill.setColor(Color.WHITE);
+        white_paintbrush_fill.setStyle(Paint.Style.FILL);
 
-    //Penguin nose
-    triangle = new Path();
-    triangle.moveTo(145,145);
-    triangle.lineTo(155,145);
-    triangle.moveTo(155,145);
-    triangle.lineTo(160,160);
-    triangle.moveTo(160,160);
-    triangle.lineTo(145,145);
-    canvas.drawPath(triangle, yellow_paintbrush_fill);
+        canvas.drawCircle(100, 100, 50, black_paintbrush_fill); //body
+        canvas.drawCircle(125, 125, 5, white_paintbrush_fill); //left eye
+        canvas.drawCircle(175, 125, 5, white_paintbrush_fill); //right eye
+
+        //Penguin nose
+        nose = new Path();
+        nose.moveTo(145, 145);
+        nose.lineTo(155, 145);
+        nose.moveTo(155, 145);
+        nose.lineTo(160, 160);
+        nose.moveTo(160, 160);
+        nose.lineTo(145, 145);
+        canvas.drawPath(nose, yellow_paintbrush_fill);
+
+        //Penguin left arm
+        leftArm = new Path();
+        leftArm.moveTo(100, 150);
+        leftArm.lineTo(90, 185);
+        leftArm.moveTo(90, 185);
+        leftArm.lineTo(135, 185);
+        leftArm.moveTo(135, 185);
+        leftArm.lineTo(100, 150);
+        canvas.drawPath(leftArm, black_paintbrush_fill);
+
+        //Penguin right arm
+        rightArm = new Path();
+        rightArm.moveTo(200, 150);
+        rightArm.lineTo(210, 185);
+        rightArm.moveTo(210, 185);
+        rightArm.lineTo(185, 185);
+        rightArm.moveTo(185, 185);
+        rightArm.lineTo(200, 150);
+        canvas.drawPath(rightArm, black_paintbrush_fill);
+
+        //Penguin left foot
+        leftFoot = new Path();
+        leftFoot.moveTo(120, 195);
+        leftFoot.lineTo(130, 195);
+        leftFoot.moveTo(130, 195);
+        leftFoot.lineTo(125, 205);
+        leftFoot.moveTo(125, 205);
+        leftFoot.lineTo(120, 195);
+        canvas.drawPath(leftFoot, yellow_paintbrush_fill);
+
+        //Penguin right foot
+        rightFoot = new Path();
+        rightFoot.moveTo(170, 195);
+        rightFoot.lineTo(180, 195);
+        rightFoot.moveTo(180, 195);
+        rightFoot.lineTo(155, 205);
+        rightFoot.moveTo(155, 205);
+        rightFoot.lineTo(170, 195);
+        canvas.drawPath(rightFoot, yellow_paintbrush_fill);
+    }
 }
